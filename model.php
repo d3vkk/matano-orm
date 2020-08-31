05941e3968e942717645fd5ce1a8f35dfc7e781d
d3vkk <donaldkk22@yahoo.com>
2020-08-31 11:06:22 +0300

    refactor: Add example function to model class

--- model.php
+++ model.php
@@ -6,6 +6,21 @@ class Model extends Matano
 {
 
 
+    /**
+     * Add user profile
+     *
+     * @param  string  $firstname
+     * @param  string  $lastname
+     * @param  string  $phone
+     * @return boolean
+     */
+    public function addUserProfile($firstname, $lastname, $phone)
+    {
+        return $this->insert("INSERT INTO users (firstname, lastname, phone) VALUES ('$firstname', '$lastname', '$phone');", 'users');
+
+    }//end addUserProfile()
+
+
 }//end class
 
 $db = new Model('host', 'user', 'password', 'database');
